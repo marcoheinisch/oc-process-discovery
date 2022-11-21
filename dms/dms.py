be84f275ae6500d6cac9142bf64032e435277913
pgcampana13 <117573440+pgcampana13@users.noreply.github.com>
2022-11-21 16:14:38 +0100

    Create dms.py

---
+++ dms/dms.py
@@ -0,0 +1,15 @@
+class SingletonClass(object):
+  def __new__(cls):
+    if not hasattr(cls, 'instance'):
+      cls.instance = super(SingletonClass, cls).__new__(cls)
+    return cls.instance
+
+singleton = SingletonClass()
+
+class DataManagementSystem():
+  def store_in_dms(data):
+    SingletonClass.singleton = data
+
+  def load_from_dms():
+    return SingletonClass.singleton
+
