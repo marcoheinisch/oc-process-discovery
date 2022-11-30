b7d8d5698a5ad30880551ae34c7ad085dfdae54f
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2022-11-30 16:21:15 +0100

    Pre merge quick fixes

--- pages/analyse/process_discovery.py
+++ pages/analyse/process_discovery.py
@@ -11,23 +11,23 @@ from pm4py.objects.ocel.constants import OCEL_GLOBAL_EVENT
 
 def ocpa_discover():
     # delete those two lines later on (only for testing purposes until a proper ocel extraction and data management interfaces are in place
-    filename="test/resources/order_process.jsonocel"
+    filename="data/resources/order_process.jsonocel"
     ocel = ocel_import_factory.apply(file_path=filename)
     ocpn = ocpn_discovery_factory.apply(ocel, parameters={"debug": False})
-    ocpn_vis_factory.save(ocpn_vis_factory.apply(ocpn), "test/results/oc_petri_net_ocpa.png")
+    ocpn_vis_factory.save(ocpn_vis_factory.apply(ocpn), "data/results/oc_petri_net_ocpa.png")
     dot = ocpn_vis_factory.apply(ocpn).source
     return dot
 
 def pm4py_discover():
     # delete those two lines later on (only for testing purposes until a proper ocel extraction and data management interfaces are in place
     print(1)
-    filename = "test/resources/order_process.jsonocel"
+    filename = "data/resources/order_process.jsonocel"
     print(2)
     ocel = pm4py.read_ocel(file_path=filename)
     print(3)
     ocpn = pm4py.discover_oc_petri_net(ocel)
     print(4)
-    pm4py.save_vis_ocpn(ocpn, "test/results/oc_petri_net_pm4py.png")
+    pm4py.save_vis_ocpn(ocpn, "data/results/oc_petri_net_pm4py.png")
     print(5)
     from pm4py.visualization.ocel.ocpn import visualizer as ocpn_visualizer
     print(6)
