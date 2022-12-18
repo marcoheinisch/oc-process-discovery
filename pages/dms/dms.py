3ee570f134f6c448893ddf7fa4b4aedea708523f
Jean-Christophe <117567212+Jean-Christophe-S@users.noreply.github.com>
2022-12-18 15:34:09 +0100

    Upload and view files on dm page

--- pages/dms/dms.py
+++ pages/dms/dms.py
@@ -1,36 +1,72 @@
-from dash import html
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
-
-
-start_time_str = dcc.Input(id="start-time", type="text", placeholder="Start time (hh:mm:ss)")
-end_time_str = dcc.Input(id="end-time", type="text", placeholder="End time (hh:mm:ss)")
+import datetime
+from dash import dash, dcc, html
+from dash.dependencies import Input, Output, State
+from dms.dms import DataManagementSystem
+from app import app
 
 
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+#start_time_str = dcc.Input(id="start-time", type="text", placeholder="Start time (hh:mm:ss)")
+#end_time_str = dcc.Input(id="end-time", type="text", placeholder="End time (hh:mm:ss)")
+
+
+
+
+# Layout for the file upload component
 layout = html.Div([
+    # Header
     html.H1("Dataset Management"),
     html.Hr(),
+    
+    # Extract from SAP button
     html.Div([
+        html.Button('Extract from SAP', id='btn-extract', n_clicks=0),
+        html.Div(id='container-feedback-text')
+    ], style={'width': '30%', 'display': 'inline-block', 'padding': '10px'}),
+    
+    # File upload component
+    html.Div([
+        dcc.Upload(
+            id='upload-jsonocel',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files')
+            ]),
+            style={
+                'width': '15%',
+                'height': '40px',
+                'lineHeight': '40px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+            # Allow multiple files to be uploaded
+            multiple=True
+        ),
+        html.Div(id='output-jsonocel-upload'),
+    ], style={'padding': '10px'}),
+    
+    # Data management component
+    html.Div([
+        html.H6("View and delete files"),
+        html.Div(
+             html.Div([
+            html.Div(html.B("Uploaded Files")),
+            dcc.Checklist(id='uploaded-files-checklist', options=[])
+        ], style={'width': '25%', 'display': 'inline-block'}),
+        style={'color': 'grey', 'height': '300px', 'width': '300%', 'display': 'flex', 'justify-content': 'left'}),
         html.Div([
-            html.Button('Extract from SAP', id='btn-extract', n_clicks=0),
-            html.Div(id='container-feedback-text')
-        ], style={'width': '30%', 'display': 'inline-block'}),
-        html.Div([
-            html.H6("Preview and Filter"),
-            html.Div(
-                "Preview dataset information and metrics",
-            style={'color': 'grey', 'height': '300px', 'display': 'flex', 'justify-content': 'center'}),
             html.Div([
-                html.Div([
-                    html.Div(html.B("Objects"), style={'width': '25%', 'display': 'inline-block'}),
-                    dcc.Checklist(
-                        options=[
-                            {'label': 'Option 1', 'value': 'option-1'},
-                            {'label': 'Option 2', 'value': 'option-2'},
-                            {'label': 'Option 3', 'value': 'option-3'}
+                html.Div(html.B("Objects"), style={'width': '25%', 'display': 'inline-block'}),
+                dcc.Checklist(
+                    options=[
+                        {'label': 'Option 1', 'value': 'option-1'},
+                        {'label': 'Option 2', 'value': 'option-2'},
+                        {'label': 'Option 3', 'value': 'option-3'},
                         ],
                         value=['option-1', 'option-3'],
                         labelStyle={'display': 'block'}
@@ -49,17 +85,55 @@ layout = html.Div([
                     ),
                 ], style={'width': '25%', 'display': 'inline-block'}),
 
-            
+            html.Hr(),
 
                 html.Div([
                     html.B("Timestamp"),
-                    start_time_str,
-                    end_time_str
-                ], style={'width': '25%', 'display': 'inline-block'}),
-
+                    dcc.DatePickerRange(
+                    id='date-picker-range',
+                    start_date_placeholder_text="Start Period",
+                    end_date_placeholder_text="End Period",
+                    
+                    )],
+        )
                 
             ])
-        ], style={'width': '33%', 'display': 'inline-block', 'vertical-align': 'top'})
+        ], style={'width': '33%', 'display': 'inline-block', 'vertical-align': 'top', 'padding': '10px'}),
     ])
-])
 
+
+# Callback function to store the contents of the uploaded file
+@app.callback(Output('output-jsonocel-upload', 'children'),
+              [Input('upload-jsonocel', 'contents')],
+              [State('upload-jsonocel', 'filename'),
+               State('upload-jsonocel', 'last_modified')])
+def parse_contents(contents, filename, date):
+    # Store the contents of the uploaded file
+    DataManagementSystem.store(contents)
+    return html.Div([
+        'File {} successfully uploaded'.format(filename)
+    ])
+
+
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    # Check if a file was uploaded
+    if list_of_contents is not None:
+        # Initialize an list called children
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+#list of uploaded files
+@app.callback(Output('uploaded-files-checklist', 'options'),
+              [Input('upload-jsonocel', 'filename')])
+def update_checklist_options(filenames):
+    if filenames is not None:
+        return [{'label': filename, 'value': filename} for filename in filenames]
+    return []
+
+
+
+
+if __name__ == '__main__': #only run if this file is called directly
+    app.run_server(debug=True) #enables debug mode
--- pages/home/home.py
+++ pages/home/home.py
@@ -7,55 +7,6 @@ from app import app
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-#layout = html.Div(html.P("This is the content of the Home page!"))
+layout = html.Div(html.P("This is the content of the Home page!"))
 
-#app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-layout = html.Div([
-    dcc.Upload(
-        id='upload-jsonocel',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-    html.Div(id='output-jsonocel-upload'),
-])
-
-def parse_contents(contents, filename, date):
-    DataManagementSystem.store(contents)
-    if 'jsonocel' in filename: 
-        return html.Div([
-            html.H5(filename),
-            html.H6(datetime.datetime.fromtimestamp(date)),
-            html.H6('File successfully imported.'),
-            html.Hr(),
-        ])
-    else:
-        return html.Div(['Please upload a valid .jsonocel file.'])
-
-@app.callback(Output('output-jsonocel-upload', 'children'),
-              Input('upload-jsonocel', 'contents'),
-              State('upload-jsonocel', 'filename'),
-              State('upload-jsonocel', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
