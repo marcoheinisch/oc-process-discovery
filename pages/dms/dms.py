db6a4f0ec690cf5321a9201426f7fc98b0a7b7fb
Jean-Christophe <117567212+Jean-Christophe-S@users.noreply.github.com>
2022-12-19 00:16:21 +0100

    Deleted Filter GUI, bug fixes, added delete button.

--- pages/dms/dms.py
+++ pages/dms/dms.py
@@ -31,8 +31,7 @@ layout = html.Div([
         dcc.Upload(
             id='upload-jsonocel',
             children=html.Div([
-                'Drag and Drop or ',
-                html.A('Select Files')
+                html.A('Upload .jsonocel files')
             ]),
             style={
                 'width': '15%',
@@ -45,7 +44,10 @@ layout = html.Div([
                 'margin': '10px'
             },
             # Allow multiple files to be uploaded
-            multiple=True
+            multiple=True,
+            # Only allow files with the .jsonocel extension to be selected
+            accept=".jsonocel"
+
         ),
         html.Div(id='output-jsonocel-upload'),
     ], style={'padding': '10px'}),
@@ -56,50 +58,14 @@ layout = html.Div([
         html.Div(
              html.Div([
             html.Div(html.B("Uploaded Files")),
-            dcc.Checklist(id='uploaded-files-checklist', options=[])
+            dcc.Checklist(id='uploaded-files-checklist', options=[]),
+            html.Button(id='delete-file-button', children='Delete')
         ], style={'width': '25%', 'display': 'inline-block'}),
         style={'color': 'grey', 'height': '300px', 'width': '300%', 'display': 'flex', 'justify-content': 'left'}),
-        html.Div([
-            html.Div([
-                html.Div(html.B("Objects"), style={'width': '25%', 'display': 'inline-block'}),
-                dcc.Checklist(
-                    options=[
-                        {'label': 'Option 1', 'value': 'option-1'},
-                        {'label': 'Option 2', 'value': 'option-2'},
-                        {'label': 'Option 3', 'value': 'option-3'},
-                        ],
-                        value=['option-1', 'option-3'],
-                        labelStyle={'display': 'block'}
-                    ),
-                ], style={'width': '25%', 'display': 'inline-block'}),
-                html.Div([
-                    html.Div(html.B("Activities"), style={'width': '25%', 'display': 'inline-block'}),
-                    dcc.Checklist(
-                        options=[
-                            {'label': 'Option A', 'value': 'option-a'},
-                            {'label': 'Option B', 'value': 'option-b'},
-                            {'label': 'Option C', 'value': 'option-c'}
-                        ],
-                        value=['option-a', 'option-c'],
-                        labelStyle={'display': 'block'}
-                    ),
-                ], style={'width': '25%', 'display': 'inline-block'}),
-
-            html.Hr(),
-
-                html.Div([
-                    html.B("Timestamp"),
-                    dcc.DatePickerRange(
-                    id='date-picker-range',
-                    start_date_placeholder_text="Start Period",
-                    end_date_placeholder_text="End Period",
-                    
-                    )],
-        )
-                
-            ])
+        
         ], style={'width': '33%', 'display': 'inline-block', 'vertical-align': 'top', 'padding': '10px'}),
     ])
+    
 
 
 # Callback function to store the contents of the uploaded file
@@ -107,33 +73,51 @@ layout = html.Div([
               [Input('upload-jsonocel', 'contents')],
               [State('upload-jsonocel', 'filename'),
                State('upload-jsonocel', 'last_modified')])
+#store the contents of an uploaded file and display a message indicating the file was successfully uploaded
 def parse_contents(contents, filename, date):
-    # Store the contents of the uploaded file
     DataManagementSystem.store(contents)
     return html.Div([
         'File {} successfully uploaded'.format(filename)
     ])
 
 
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    # Check if a file was uploaded
-    if list_of_contents is not None:
-        # Initialize an list called children
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
 
 #list of uploaded files
 @app.callback(Output('uploaded-files-checklist', 'options'),
-              [Input('upload-jsonocel', 'filename')])
-def update_checklist_options(filenames):
+              [Input('upload-jsonocel', 'filename')],
+              [State('uploaded-files-checklist', 'options')])
+def update_checklist_options(filenames, existing_options):
     if filenames is not None:
-        return [{'label': filename, 'value': filename} for filename in filenames]
-    return []
+        # Append the filename of the most recently uploaded file to the existing options
+        updated_options = existing_options + [{'label': filename, 'value': filename} for filename in filenames]
+        return updated_options
+    return existing_options
 
+""" THIS IS TO DELETE FILES BUT NEEDS SOME DEBUGGING
+# list of uploaded files and deleted files
+@app.callback(Output('uploaded-files-checklist', 'options'),
+              [Input('upload-jsonocel', 'filename'),
+               Input('delete-file-button', 'n_clicks')],
+              [State('uploaded-files-checklist', 'options'),
+               State('uploaded-files-checklist', 'value')])
+def update_checklist_options(filenames, delete_clicks, existing_options, selected_options):
+    print(filenames, delete_clicks, existing_options, selected_options)
+    ctx = dash.callback_context
+    if ctx.triggered:
+        trigger = ctx.triggered[0]['prop_id'].split('.')[0]
 
+    if trigger == 'upload-jsonocel':
+        # Append the filename of the most recently uploaded file to the existing options
+        updated_options = existing_options + [{'label': filename, 'value': filename} for filename in filenames]
+    elif trigger == 'delete-file-button':
+        # Remove the selected files from the existing options
+        updated_options = [option for option in existing_options if option['value'] not in selected_options]
+    else:
+        # Return the existing options if no updates are made
+        updated_options = existing_options
+    return updated_options
 
+"""
 
 if __name__ == '__main__': #only run if this file is called directly
     app.run_server(debug=True) #enables debug mode
