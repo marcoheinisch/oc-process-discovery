e342bd788984e1bc82dde74591b7eb56b4c9fb80
Kacper Kuca <66218711+kap5el14@users.noreply.github.com>
2022-12-18 15:05:21 +0100

    filtering

---
+++ filtering/playground.py
@@ -0,0 +1,152 @@
+from dash.dependencies import Input, Output, State
+import copy
+import os
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pm4py
+from pm4py.objects.ocel.obj import OCEL
+
+original_path = None
+log_paths = []
+ocel = None
+
+# should be triggered by sap extraction callback
+
+
+def prepare_for_filtering():
+    global original_path
+    original_path = 'test/p2p-normal.jsonocel'  # change later to the path of the extracted jsonocel
+    global log_paths
+    log_paths.append(copy.deepcopy(original_path))
+    global ocel
+    ocel = pm4py.read_ocel(get_path())
+
+
+def get_path():
+    return log_paths[len(log_paths) - 1]
+
+
+def get_ocel() -> OCEL:
+    return pm4py.read_ocel(get_path())
+
+
+def get_new_path_name():
+    length = len(log_paths)
+    if length == 1:
+        return 'test/filtered_ocel.json'
+    else:
+        return 'test/filtered_ocel{}.json'.format(length - 1)
+
+def save_filtered_ocel(ocel):
+    path = get_new_path_name()
+    pm4py.write_ocel(ocel, path)
+    log_paths.append(path)
+
+
+prepare_for_filtering()
+app = dash.Dash()
+
+
+# panel components
+# Filter on Event Attributes
+event_attribute_dropdown = dcc.Dropdown(
+    id='event-attribute-dropdown',
+    options=[{'label': attr, 'value': attr} for attr in get_ocel().events.columns.tolist()],
+    value=[],
+    multi=True
+)
+
+# create checkboxes for selecting elements
+event_attribute_checkboxes = html.Div(
+    id='event-attribute-checkboxes',
+    children=[]
+)
+
+# create radio buttons for setting "Positive" flag
+event_attribute_positive_radio = dcc.RadioItems(
+    id='event-attribute-positive-radio',
+    options=[{'label': 'positive', 'value': 'True'}, {'label': 'negative', 'value': 'False'}],
+    value='True',  # default value
+    labelStyle={'display': 'inline-block'}
+)
+
+
+# callbacks
+# Filter on Event Attributes callbacks
+# define callback for updating element checkboxes
+@app.callback(
+    Output('event-attribute-checkboxes', 'children'),
+    [Input('event-attribute-dropdown', 'value')]
+)
+def update_event_attribute_checkboxes(keys):
+    checkboxes = []
+    for key in keys:
+        checkboxes.append(html.Div(
+            children=[
+                dcc.Checklist(
+                    id=f'{key}-checklist',
+                    options=[{'label': element, 'value': element} for element in sorted(set(get_ocel().events[key]))],
+                    value=[]
+                )
+            ]
+        ))
+    return checkboxes
+
+
+# define callback for updating "Positive" flag
+@app.callback(
+    Output('event-attribute-positive-flag', 'children'),
+    [Input('event-attribute-positive-radio', 'value')]
+)
+def update_event_attribute_positive_flag(value):
+    return value
+
+
+
+
+
+
+
+
+
+
+# define callback for rollback
+@app.callback(
+    [
+        Output('event_attribute_dropdown', 'value'),
+        Output('event-attribute-checkboxes', 'value'),
+        Output('event-attribute-positive-radio', 'value'),
+    ],
+    Input('rollback-button', 'n_clicks')
+)
+def rollback(n_clicks):
+    path = get_path()
+    # don't do the rollback if no filtering has been done
+    if path == original_path:
+        return
+    # delete current filtered ocel file
+    os.remove(get_path())
+    del log_paths[len(log_paths) - 1]
+
+    # update the current ocel
+    global ocel
+    ocel = pm4py.read_ocel(get_path())
+
+    # reset filtering options to default values
+    return None, None, None
+
+
+# create layout
+app.layout = html.Div(
+    [
+        event_attribute_dropdown,
+        event_attribute_checkboxes,
+        event_attribute_positive_radio,
+        html.Button('Rollback', id='rollback-button')
+    ],
+    id='filtering_panel'
+)
+
+if __name__ == '__main__':
+    app.run_server()
