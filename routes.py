9fd1b51a18ab543e6c21c06539494bc145c1d1eb
Kacper Kuca <66218711+kap5el14@users.noreply.github.com>
2023-01-06 11:32:18 +0100

    one files is always selected

--- routes.py
+++ routes.py
@@ -8,17 +8,21 @@ from utils.constants import home_page_location, dms_page_location, analyse_page_
 
 from pages.home import home
 from pages.dms import dms
+from dms.dms import SingletonClass
 from pages.analyse import analyse
 
-
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+@app.callback(
+    Output("page-content", "children"),
+    Output('uploaded-files-checklist', 'value'),
+    [Input("url", "pathname")])
 def render_page_content(pathname):
+    selected = SingletonClass().selected
     if pathname == home_page_location:
-        return home.layout
+        return home.layout, selected
     elif pathname == dms_page_location:
-        return dms.layout
+        return dms.layout, selected
     elif pathname == analyse_page_location:
-        return analyse.layout
+        return analyse.layout, selected
     # If the user tries to reach a different page, return a 404 message
     return dbc.Jumbotron(
         [
