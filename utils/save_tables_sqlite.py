f4e082e75995f8f037d708e3e4ca1e158e0bb5c5
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2023-01-25 18:15:51 +0100

    moved sqlite db for docker

--- utils/save_tables_sqlite.py
+++ utils/save_tables_sqlite.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import sqlite3
 
-from utils.sap_con import SapConnector
-from utils import constants
-from environment.settings import SAP_CON_PARAMS
+from sap_con import SapConnector
+from pyrfc import Connection, ABAPApplicationError, ABAPRuntimeError, LogonError, CommunicationError
+
 
 SQLITE_PATH = "sap_tables.sqlite"
 
@@ -20,23 +20,35 @@ SAP_CON_PARAMS = {
     'trace': "SAP_TRACE"
 }
 
+tables = {
+    'BSAD': [11, 'MANDT', 'BUKRS', "KUNNR", "UMSKS", "UMSKZ", 'AUGDT', 'AUGBL', "ZUONR", 'GJAHR', 'BELNR', 'BLART',
+             'VBELN'],
+    'VBFA': [0, "ERDAT", "ERZET", "VBELN", "VBELV", "VBTYP_N", "VBTYP_V", "RFMNG", "MEINS", "RFWRT", "WAERS", "MATNR",
+             "BWART", "VRKME", "FKTYP", "POSNN", "POSNV"],
+    'VBAK': [0, "VBELN", "ERDAT", "ERZET", "KUNNR"],
+    'CDHDR': [4, 'MANDANT', 'OBJECTCLAS', 'OBJECTID', 'CHANGENR', 'USERNAME', 'UDATE', 'UTIME', 'TCODE', 'CHANGE_IND'],
+    'CDPOS': [8, 'MANDANT', 'OBJECTCLAS', 'OBJECTID', 'CHANGENR', 'TABNAME', 'TABKEY', 'FNAME', 'CHNGIND'],
+}
+
 
 def save_tables():
     """Function to save SAP data tables in sql. For debugging purposes.
     """
     sap_con = SapConnector(SAP_CON_PARAMS)
-    tables = {}
+    tmp = {}
     for table, fields in constants.tables.items():
         results, headers = sap_con.qry(
-            fields, table, MaxRows=constants.ROWS_AT_A_TIME)
+            fields, table, MaxRows=3000)
         df = pd.DataFrame(results, columns=headers)
-        tables[table] = df
+        tmp[table] = df
     del sap_con
-    
-    sql_con = sqlite3.connect(SQLITE_PATH)  
-    for table, df in tables.items():
+
+    sql_con = sqlite3.connect(SQLITE_PATH)
+    for table, df in tmp.items():
         table_name = str(table).upper()
         df.to_sql(table_name, sql_con, if_exists='replace')
     sql_con.close()
-    sql_con.push_tables(tables)
-    
+
+
+if __name__ == "__main__":
+    save_tables()
