806d97c03798cb27c33f745616e6c5fb49dace3d
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2023-02-01 17:24:24 +0100

    add tooltip

--- pages/analyse/analyse.py
+++ pages/analyse/analyse.py
@@ -10,12 +10,25 @@ pm_page_content = html.Div([
         html.H1("Process Analysis"),
         html.Hr(),
         html.Div(
-                    [
-                        dig(
-                            id="process-model",
-                        )
-                    ]
+            [
+                html.Span(
+                    "What do I see here?",
+                    id="tooltip-target",
+                    style={"textDecoration": "underline", "cursor": "pointer"},
+                ),
+                dbc.Tooltip(
+                    html.P("In these Process Graphs you can see the process flow of SAP documents, manly extracted from VBFA, CDHDR, BSAD, VBAK"),
+                    target="tooltip-target",
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                dig(
+                    id="process-model",
                 )
+            ]
+        )
     ],
     id="pm-page-content",    
 )
